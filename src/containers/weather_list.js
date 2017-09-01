2ce453671f2329e5b3bbfef40c28b3ea2848ac37
briecoyle <brie.coyle@gmail.com>
2017-09-01 17:27:02 -0400

    Add units to charts.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
-    console.log(cityData)
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
     return (
-      <tr key={cityData.city.name}>
-        <td>{cityData.city.name}</td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color='red' units='K' />
+        </td>
+        <td>
+          <Chart data={pressures} color='green' units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color='blue' units="%" />
+        </td>
       </tr>
     )
   }
@@ -17,9 +31,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
